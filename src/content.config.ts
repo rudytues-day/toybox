e315acd9cebc1fd34905863a3bf0ded92a3e5038
rudy tuesday <rudolfdienstag@gmail.com>
2024-12-27 22:49:11 -0500

    build music pages and schema
    - created MusicLayout.astro
    - created Playlist.astro and music-details.css
    - created /music, /music/log, /music/playlists
    - created playlist schema

---
+++ src/content.config.ts
@@ -0,0 +1,38 @@
+import { glob } from 'astro/loaders';
+import { string } from 'astro/zod';
+import { defineCollection, z, type SchemaContext } from 'astro:content';
+
+// 1. Import utilities from `astro:content`
+
+// 2. Define your collection(s)
+
+const allPlaylists = defineCollection({
+    loader: glob({ pattern: '**/*.md', base: './src/content/playlists/'}),
+    schema: z.object({
+        title: z.string().optional(),
+        date: z.date().optional(),
+    })
+});
+
+const allMusicLogs = defineCollection({
+    loader: glob({ pattern: '**/*.md', base: './src/content/playlists/music-logs/'}),
+    schema: z.object({
+        title: z.string().optional(),
+        date: z.date().optional(),
+    })
+});
+
+const allCuratedLists = defineCollection({
+    loader: glob({ pattern: '**/*.md', base: './src/content/playlists/curated/'}),
+    schema: z.object({
+        title: z.string().optional(),
+        date: z.date().optional(),
+    })
+});
+
+
+export const collections = {
+    'playlists': allPlaylists,
+    'musicLogs': allMusicLogs,
+    'curatedLists': allCuratedLists
+}
